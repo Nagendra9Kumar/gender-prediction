cb75869552df76c50da5e7769eba0c505fdd8360
Nagendra <nagendragubbala999@gmail.com>
2024-07-30 22:53:39 +0530

    requriments

--- api/model.py
+++ api/model.py
@@ -1,11 +1,11 @@
 import pickle
 
 def load_model():
-    with open('model/model.pkl', 'rb') as f:
+    with open('https://github.com/Nagendra9Kumar/gender-prediction/blob/main/model/model.pkl?raw=true', 'rb') as f:
         model = pickle.load(f)
     return model
 
 def load_vectorizer():
-    with open('model/vectorizer.pkl', 'rb') as f:
+    with open('https://github.com/Nagendra9Kumar/gender-prediction/blob/main/model/predict.pkl?raw=true', 'rb') as f:
         vectorizer = pickle.load(f)
     return vectorizer
